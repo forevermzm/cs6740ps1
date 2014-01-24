d50ccfd57fa6a712bc0f50f49f59e7dcc0a4075b
zhemin <forevermzm00@gmail.com>
2014-01-23 21:55:26 -0500

    Added a mechanism in server to check if a client is greeted. Also changed the main method to ChatClient instead of ReceivingThread

--- ChatClient.java
+++ ChatClient.java
@@ -1,34 +1,34 @@
 import java.io.*;
 import java.net.*;
 
-public class ChatClient extends Thread{
+public class ChatClient extends Thread {
     private DatagramSocket socket;
     private InetAddress address;
     private int portNumber;
     private BufferedReader stdIn = new BufferedReader( new InputStreamReader(System.in));
 
-    public ChatClient(InetAddress address, int portNumber) throws Exception{
+    public ChatClient(InetAddress address, int portNumber) throws Exception {
         this.socket = new DatagramSocket(null);
         this.address = address;
         this.portNumber = portNumber;
     }
 
-    private synchronized void sendMessage(String message) throws Exception{
+    private synchronized void sendMessage(String message) throws Exception {
         // System.out.println(message);
         byte[] data = message.getBytes();
         DatagramPacket packet = new DatagramPacket (data, data.length, address, portNumber);
         socket.send(packet);
         // notify();
-    }    
+    }
 
-    private void sendMessage() throws Exception{
+    private void sendMessage() throws Exception {
         String userInput;
         if ((userInput = stdIn.readLine()) != null) {
             sendMessage("MESSAGE " + userInput);
         }
     }
 
-    public synchronized void getMessage(){
+    public synchronized void getMessage() {
         try {
             socket.setSoTimeout(1000);
             byte[] buf = new byte[1024];
@@ -37,15 +37,15 @@ public class ChatClient extends Thread{
             socket.receive(packet);
             // System.out.println("It is reaching me here.");
             System.out.println(new String(packet.getData()));
-        } catch (SocketTimeoutException e){
+        } catch (SocketTimeoutException e) {
             // System.out.println("It's timed out.");
             // e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        } 
+        }
     }
 
-    public void run(){
+    public void run() {
         System.out.println("ChatClient is started.");
         try {
             sendMessage("GREETING message");
@@ -54,47 +54,48 @@ public class ChatClient extends Thread{
                 // System.out.println("Send Message: ");
                 sendMessage();
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println(
+                "Usage: java ChatClient <host name> <port number>");
+            System.exit(1);
+        }
+        try {
+            InetAddress address = InetAddress.getByName(args[0]);
+            int portNumber = Integer.parseInt(args[1]);
+
+            ChatClient chatClient = new ChatClient(address, portNumber);
+            new ReceivingThread(chatClient).start();
+            chatClient.start();
+        } catch (UnknownHostException e) {
+            System.out.println("Host is not found!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
 class ReceivingThread extends Thread {
-        private ChatClient chatClient;
+    private ChatClient chatClient;
 
-        public ReceivingThread (ChatClient chatClient){
-            this.chatClient = chatClient;
-        }
+    public ReceivingThread (ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
 
-        public static void main(String[] args){
-            if (args.length != 2) {
-                System.err.println(
-                    "Usage: java ChatClient <host name> <port number>");
-                System.exit(1);
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(1);
+                chatClient.getMessage();
             }
-            try {
-                InetAddress address = InetAddress.getByName(args[0]);
-                int portNumber = Integer.parseInt(args[1]);
-
-                ChatClient chatClient = new ChatClient(address, portNumber);
-                new ReceivingThread(chatClient).start();
-                chatClient.start();
-            } catch (UnknownHostException e){
-                System.out.println("Host is not found!");
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-
-        public void run(){
-            try {
-                while (true){
-                    Thread.sleep(1);
-                    chatClient.getMessage();
-                }
-            } catch (Exception e){
-                System.out.println("I'm interuptted.");
-            }
+        } catch (Exception e) {
+            System.out.println("I'm interuptted.");
         }
     }
+}
