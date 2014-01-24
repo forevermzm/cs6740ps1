b1d95894eb67e6e31e8dce6e0f3c2913e7f8e798
zhemin <forevermzm00@gmail.com>
2014-01-23 21:56:26 -0500

    Added a mechanism in server to check if a client is greeted.

--- ChatServer.java
+++ ChatServer.java
@@ -6,42 +6,46 @@ public class ChatServer {
     private DatagramSocket socket;
     private Set<GreetedClient> greetedClients = new HashSet<GreetedClient>();
 
-    public ChatServer(int port){
+    public ChatServer(int port) {
         try {
             socket = new DatagramSocket(port);
-        } catch( Exception ex ){
+        } catch ( Exception ex ) {
             System.out.println("Problem creating socket.");
         }
     }
 
-    public void start() throws Exception{
+    public void start() throws Exception {
         System.out.println("Server Initialized...");
-        while (true){
+        while (true) {
             byte[] buf = new byte[1024];
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
             String packetMessage = new String(packet.getData());
-            // System.out.println("PacketMessage: " + packetMessage + " from: " + packet.getPort());
+            System.out.println("PacketMessage: " + packetMessage + " from: " + packet.getPort());
             if (packetMessage == null) {
                 continue;
-            } else if (isGreetingMessage(packetMessage)){
+            } else {
+                Thread.sleep(10000);
                 GreetedClient greetedClient = new GreetedClient(packet);
-                greetedClients.add(greetedClient);
-                // System.out.println("HashSet size: " + greetedClients.size());
-            } else if (isMessage(packetMessage)){
-                sendIncomingMessage(packet);
+                if (isGreetingMessage(packetMessage)) {
+                    greetedClients.add(greetedClient);
+                    // System.out.println("HashSet size: " + greetedClients.size());
+                } else if (isMessage(packetMessage)) {
+                    if (greetedClients.contains(greetedClient))
+                        sendIncomingMessage(packet);
+                }
             }
         }
     }
 
-    private void sendIncomingMessage(DatagramPacket packet){
+    private void sendIncomingMessage(DatagramPacket packet) {
         String ip = packet.getAddress().getHostAddress();
         String port = Integer.toString(packet.getPort());
         String message = new String(packet.getData());
         message = message.substring(8);
 
         String incomingMessage = "<From " + ip + ":" + port + ">: " + message;
-        for (GreetedClient greetedClient: greetedClients){
+        for (GreetedClient greetedClient : greetedClients) {
             try {
                 socket.send(greetedClient.generateINCOMINGPacket(incomingMessage.getBytes()));
             } catch (IOException e) {
@@ -50,15 +54,15 @@ public class ChatServer {
         }
     }
 
-    private boolean isGreetingMessage(String packetMessage){
+    private boolean isGreetingMessage(String packetMessage) {
         return packetMessage.contains("GREETING");
     }
 
-    private boolean isMessage(String packetMessage){
+    private boolean isMessage(String packetMessage) {
         return packetMessage.contains("MESSAGE");
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.err.println(
                 "Usage: java ChatServer <port number>");
@@ -74,18 +78,17 @@ public class ChatServer {
         private InetAddress address;
         private int port;
 
-        public GreetedClient(DatagramPacket packet){
+        public GreetedClient(DatagramPacket packet) {
             address = packet.getAddress();
             port = packet.getPort();
         }
 
         @Override
-        public boolean equals(Object obj) 
-        {
+        public boolean equals(Object obj) {
             if (obj == null) return false;
             if (obj instanceof GreetedClient) {
                 GreetedClient anotherClient = (GreetedClient) obj;
-                if ( this.address.equals(anotherClient.getAddress()) && this.port == anotherClient.getPort() ) 
+                if ( this.address.equals(anotherClient.getAddress()) && this.port == anotherClient.getPort() )
                     return true;
             }
             return false;
@@ -98,18 +101,18 @@ public class ChatServer {
             result = prime * result + address.hashCode();
             result = prime * result + (new Integer(port).hashCode());
             return result;
-       }
+        }
 
-        public DatagramPacket generateINCOMINGPacket(byte[] message){
+        public DatagramPacket generateINCOMINGPacket(byte[] message) {
             // System.out.println("Preparing to send " + (new String(message)));
             return new DatagramPacket(message, message.length, address, port);
         }
 
-        public InetAddress getAddress(){
+        public InetAddress getAddress() {
             return address;
         }
 
-        public int getPort(){
+        public int getPort() {
             return port;
         }
     }
